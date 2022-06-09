2b580564357d4ccaa3b7918fbe21f973ec1beb79
DavidZ <davidbill@outlook.com>
2022-06-09 16:15:43 +1000

    :lipstick: close #27

--- django_monaco_editor/static/django_monaco_editor/js/editor.js
+++ django_monaco_editor/static/django_monaco_editor/js/editor.js
@@ -2,27 +2,39 @@ require.config({paths: {'vs': '/static/external/monaco-editor/min/vs'}});
 
 require(['vs/editor/editor.main'], function () {
 
-    // 实际的textarea
-    let content = document.querySelector('textarea[use-monaco-editor="true"]')
-    if (!content) {
-        return
-    }
+  // 实际的textarea
+  let contentELementList = document.querySelectorAll('textarea[use-monaco-editor="true"]')
+  if (contentELementList.length === 0) {
+    return
+  }
 
-    // 配置editor
-    let editor = monaco.editor.create(document.getElementById('editor-id'), {
+  // 配置editor
+  const editorElementList = document.querySelectorAll('#editor-id')
+  const editorList = []
+  for (let i = 0; i < editorElementList.length; i++) {
+    let editorElement = editorElementList[i]
+    let contentElement = contentELementList[i]
+    editorList.push(monaco.editor.create(editorElement, {
         theme: 'vs-dark',
         cursorBlinking: 'smooth',
         fontSize: '14px',
         language: 'json',
         minimap: {
-            enabled: true
+          enabled: true
         },
-        value: JSON.stringify(JSON.parse(content.value), null, 2),
-    });
+        value: JSON.stringify(JSON.parse(contentElement.value), null, 2),
+      })
+    )
+  }
 
-    // 监听提交表单操作，传值给实际的textarea
-    let form = content.form;
-    form.addEventListener('submit', function () {
-        content.value = editor.getValue();
-    });
+  // 监听提交表单操作，传值给实际的textarea
+  let form = contentELementList[0].form;
+  form.addEventListener('submit', function () {
+      for (let i = 0; i < editorList.length; i++) {
+        let editor = editorList[i]
+        let contentElement = contentELementList[i]
+        contentElement.value = editor.getValue();
+      }
+    }
+  )
 })
